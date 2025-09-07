5826a95e120c45defdb5e5f807728f2737f2f71c
Rhuan Barreto <rhuan@barreto.work>
2025-09-07 15:58:26 +0200

    refactor: simplify project initialization and remove unused configurations

--- .simple-release.js
+++ .simple-release.js
@@ -1,17 +1,7 @@
 import { NpmProject } from "@simple-release/npm";
 
-export const project = new NpmProject({
-  mode: "fixed",
-});
+export const project = new NpmProject();
 
 export const releaser = {
   verbose: true,
-};
-
-export const bump = {
-  extraScopes: ["deps"],
-};
-
-export const publish = {
-  access: "public",
 };
